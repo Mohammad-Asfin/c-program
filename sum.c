5492868027433b8c1d1fbb1206c7745d171ff3a5
Mohammad-Asfin <mdasfin0@gmail.com>
2024-09-30 12:30:30 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int a,b,c;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("The sum is : %d",c);
+    return 0;
+}
