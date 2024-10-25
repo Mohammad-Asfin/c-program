84c2b6914e673bbe958f3ab414e68f528c2441de
Mohammad Asfin <mdasfin0@gmail.com>
2024-10-25 12:13:38 +0530

    Create Check even or odd.c

---
+++ Check even or odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    int n;
+    printf("Enter n:\n");
+    scanf("%d",&n);
+    if(n%2 == 0)
+    {
+        printf("Even");
+    }
+    
+    else
+    {
+        printf("Odd");
+    }
+}
