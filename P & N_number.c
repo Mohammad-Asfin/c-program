e61f10130476f976883ab83570e019c482b879b4
Mohammad Asfin <mdasfin0@gmail.com>
2024-12-20 11:48:26 +0530

    Create P & N_number.c

---
+++ P & N_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    int n;
+    printf("Enter value:");
+    scanf("%d",&n);
+    if(n>0){
+        printf("Positive number");
+    }
+    else{
+        printf("Negative number");
+    }
+}
