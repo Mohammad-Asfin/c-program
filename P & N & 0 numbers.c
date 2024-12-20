040c6b3ed5c9d4e2265a8c743dd9a8e715cdce39
Mohammad Asfin <mdasfin0@gmail.com>
2024-12-20 11:45:41 +0530

    Create P & N & 0 numbers.c

---
+++ P & N & 0 numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+    int n;
+    printf("Enter value:");
+    scanf("%d",&n);
+    if(n>0){
+        printf("Positive number");
+    }
+    else if(n==0){
+         print("Equal to zero");
+    }
+    else{
+        printf("Negative number");
+    }
+}
